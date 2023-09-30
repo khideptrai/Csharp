e7e294af5284f19abff9b08f73472e12a1dcd7e8
khideptrai <nguyengiakhiem148@gmail.com>
2023-09-30 21:16:30 +0700

    update lab2_7

--- Lab2_7/Program.cs
+++ Lab2_7/Program.cs
@@ -2,12 +2,44 @@
 {
     static void Main(String[] args)
     {
-        int ngay, thang, nam;
+        int thang, nam;
+        int ngay = 0;
         Console.Write("Nhap nam:");
         nam = Convert.ToInt16(Console.ReadLine());
         Console.Write("Nhap thang:");
         thang = Convert.ToInt16(Console.ReadLine());
-        Console.WriteLine("Nam: {0} Thang {0}", nam, thang);
+        switch (thang)
+        {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                ngay = 31;
+                break;
+            case 2:
+                if(nam % 4 == 0)
+                {
+                    ngay = 29;
+                }
+                else
+                {
+                    ngay = 28;
+                }
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                ngay = 30;
+                break;
+            default:
+                Console.WriteLine("Thang khong chinh xac");
+                break;
+        }
+        Console.WriteLine("Thang {0} Nam {1} co {2} ngay",thang,nam,ngay);
         Console.Read();
 
     }
