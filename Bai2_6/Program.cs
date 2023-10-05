c9bded27f4b98e8f606685d9577629b02296b8e2
GiaKhiem <peodz666@gmail.com>
2023-10-05 11:16:48 +0700

    test

--- Bai2_6/Program.cs
+++ Bai2_6/Program.cs
@@ -2,9 +2,10 @@
 {
     static void Main(string[] args)
     {
-        for(int i = 1;i < 11; i++)
+        int n = 11;
+        for(int i = 0;i < n; i++)
         {
-            for(int j = 1; j <= 11-i; j++)
+            for(int j = 0; j <= n - i; j++)
             {
                 Console.Write(" ");
             }
