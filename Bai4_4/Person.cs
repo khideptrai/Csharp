e50fa8c0a35a67e2815305cd1e9955be31a219f1
GiaKhiem <peodz666@gmail.com>
2023-10-19 16:24:59 +0700

    lab4

--- Bai4_4/Person.cs
+++ Bai4_4/Person.cs
@@ -8,92 +8,100 @@ namespace Bai4_4
 {
     class Person
     {
-        public string Name { get; set; }
-        public int Phone { get; set; }
-        public string Email { get; set; }
+        private string name;
+        private int phone;
+        private string email;
 
         public override string ToString()
         {
-            return "Person: " + Name + ", Email: " + Email;
+            return "Person" + Name + Email;
         }
+        public string Name { get { return name; } set { name = value; } }
+        public int Phone { get { return phone; } set { phone = value; } }
+        public string Email { get { return email; } set { email = value; } }
     }
-
     class Student : Person
     {
-        public string StudyProgram { get; set; }
-
+        private string studyProgram;
         public override string ToString()
         {
-            return "Student: " + Name + ", Email: " + Email + ", Program: " + StudyProgram;
+            return "Student" + Name + Email;
         }
+        public string StudyProgram {  get { return studyProgram; } set {  studyProgram = value; } }
     }
-
     class Employee : Person
     {
-        public string Department { get; set; }
-        public double Salary { get; set; }
-        public DateTime DateHired { get; set; }
-
+        private string department;
+        private double salary;
+        private DateTime datehired;
         public override string ToString()
         {
-            return "Employee: " + Name + ", Email: " + Email;
+            return "Employee" + Name + Email;
         }
-
+        public string Department {  get { return department; } set { department = value; } }
+        public double Salary { get {  return salary; } set {  salary = value; } }
+        public DateTime Datehired { get {  return datehired; } set {  datehired = value; } }
         public virtual double CalculateBonus()
         {
             return 0;
         }
-
-        public virtual int CalculateVacation()
-        {
-            return 0;
-        }
+        public virtual int CalculateVacation() { return 0; }
     }
-
     class Faculty : Employee
     {
-        public string OfficeHours { get; set; }
-        public string Rank { get; set; }
-
+        private double officeHours;
+        private string rank;
         public override string ToString()
         {
-            return "Faculty: " + Name + ", Email: " + Email;
+            return "Faculty" + Name + Email;
         }
-
+        public double OfficeHours {  get { return officeHours; } set { officeHours = value; } }
+        public string Rank { get { return rank; } set { rank = value; } }
         public override double CalculateBonus()
         {
             return 1000 + 0.05 * Salary;
         }
-
         public override int CalculateVacation()
         {
-            int yearsEmployed = DateTime.Now.Year - DateHired.Year;
-            if (yearsEmployed >= 3)
+            if(OfficeHours >= (24*365*3))
             {
-                return (Rank == "Senior Lecturer") ? 5 : 4;
+                if(Rank == "Senior Lecturer")
+                {
+                    return 6;
+                }
+                else
+                {
+                    return 5;
+                }
             }
-            return 0;
+            else { 
+                return 4;
+            }
         }
     }
-
     class Staff : Employee
     {
-        public string Title { get; set; }
-
+        private string title;
         public override string ToString()
         {
-            return "Staff: " + Name + ", Email: " + Email;
+            return "Staff" + Name + Email;
         }
-
+        public string Title { get { return title; } set { title = value; } }
         public override double CalculateBonus()
         {
-            return 0.06 * Salary;
+            return 1000 + 0.05 * Salary;
         }
-
         public override int CalculateVacation()
         {
-            int yearsEmployed = DateTime.Now.Year - DateHired.Year;
-            return (yearsEmployed >= 5) ? 4 : 3;
+            int yearsEmployed = DateTime.Now.Year - Datehired.Year;
+            if (yearsEmployed >= 5)
+            {
+                return 4;
+            }
+            else
+            {
+                return 3;
+            }
         }
     }
 }
