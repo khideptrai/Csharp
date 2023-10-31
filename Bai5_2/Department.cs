7f6d156a773c9a113d3a0e84be13fd2b7615c588
khideptrai <nguyengiakhiem148@gmail.com>
2023-10-31 21:14:44 +0700

    lab5

---
+++ Bai5_2/Department.cs
@@ -0,0 +1,36 @@
+﻿class Department
+{
+    public string name;
+    Employee[] employees;
+    public Department() { }
+    public Department(string Name,int Capacity)
+    {
+        this.Name = Name;
+        employees = new Employee[Capacity];
+    }
+    public string Name
+    {
+        get { return name; }
+        set
+        {
+            if (value == null)
+                throw new ArgumentNullException();
+            name = value;
+        }
+    }
+    public Employee this[int index]
+    {
+        get
+        {
+            if (index < 0 || index > employees.Length - 1)
+                throw new ArgumentOutOfRangeException();
+            return employees[index];
+        }
+        set
+        {
+            if (index < 0 || index > employees.Length - 1)
+                throw new ArgumentOutOfRangeException();
+            employees[index] = value;
+        }
+    }
+}
---
+++ Bai5_2/Employee.cs
@@ -0,0 +1,28 @@
+﻿class Employee
+{
+    private int id;
+    private string name;
+    private int age;
+
+    public Employee() 
+    {
+        Id = 0;
+        Name = "";
+        Age = 0;
+    }
+    public Employee(int id, string name, int age)
+    {
+        Id = id;
+        Name = name;
+        Age = age;
+    }
+
+    public int Id { get => id; set => id = value; }
+    public string Name { get => name; set => name = value; }
+    public int Age { get => age; set => age = value; }
+
+    public override string ToString()
+    {
+        return "Id: " + Id + " Name: " + Name + " Age: " + Age;
+    }
+}
