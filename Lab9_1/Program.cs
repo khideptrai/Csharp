f4e2946011012e942f157b35894a4c4f6427f34d
khideptrai <nguyengiakhiem148@gmail.com>
2023-11-02 20:49:59 +0700

    update

---
+++ Lab9_1/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        int n, m;
+        Func<int, bool> IsPrime = delegate(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+            for (int i = 2; i < number / 2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        };
+  
+    }
+}
